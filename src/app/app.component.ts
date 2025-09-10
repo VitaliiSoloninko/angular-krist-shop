0436bc2889d652e85f214d61d570b6a8c4cc5c2e
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-09-10 14:30:03 +0200

    feat: initial project, Angular 20. Create first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from './product-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ProductCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-krist-shop';
+  product = {
+    image:
+      'https://static.pullandbear.net/2/photos//2023/I/0/2/p/6682/312/427/6682312427_2_1_8.jpg',
+    title: 'Skinny Fit Jeans',
+    rating: 3.5,
+    price: 240,
+    oldPrice: 260,
+    discount: 20,
+  };
 }
