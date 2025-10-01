4a858ab4022253e294964d23235e5866eb4c5a28
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-10-01 14:23:21 +0200

    feat(routing): set product catalog as default home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,13 @@ import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { ProductCatalogPageComponent } from './pages/product-catalog-page/product-catalog-page.component';
 import { EmptyStateTestComponent } from './pages/test-pages/empty-state-test/empty-state-test.component';
 import { ModalTestComponent } from './pages/test-pages/modal-test/modal-test.component';
-import { HeaderComponent } from './widgets/header/header/header.component';
 
 export const routes: Routes = [
-  { path: '', component: HeaderComponent },
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: '', redirectTo: 'catalog', pathMatch: 'full' },
+      { path: '', component: ProductCatalogPageComponent },
       { path: 'catalog', component: ProductCatalogPageComponent },
       { path: 'home', component: HomePageComponent },
       { path: 'cart', component: CartPageComponent },
