642207a0682248dff9317e27617f2c9337037193
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-16 07:16:09 +0100

    feat(admin - brands): add basic layout

--- src/app/pages/admin/brands-page/brands-page.component.ts
+++ src/app/pages/admin/brands-page/brands-page.component.ts
@@ -1,10 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { EmptyStateComponent } from '../../../shared/ui/empty-state/empty-state.component';
+import { Brand, BRANDS } from '../../../data/brands.data';
+import { GrayLineComponent } from '../../../shared/ui/gray-line/gray-line.component';
 
 @Component({
   selector: 'app-brands-page',
-  imports: [],
+  imports: [GrayLineComponent],
   templateUrl: './brands-page.component.html',
   styleUrl: './brands-page.component.scss',
 })
-export class BrandsPageComponent {}
+export class BrandsPageComponent {
+  brands: Brand[] = [...BRANDS];
+
+  onEditBrand(brand: Brand) {
+    console.log('Edit brand:', brand);
+  }
+
+  onDeleteBrand(brandId: number) {
+    this.brands = this.brands.filter((brand) => brand.id !== brandId);
+    console.log('Delete brand with id:', brandId);
+  }
+
+  onAddBrand() {
+    console.log('Add new brand');
+  }
+}
