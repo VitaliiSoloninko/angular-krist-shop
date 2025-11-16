6552c4079fb00aae8c28098a85a1152e8799c7f2
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-16 07:59:09 +0100

    feat(admin - products): add layout and static types

--- src/app/pages/admin/products-page/products-page.component.ts
+++ src/app/pages/admin/products-page/products-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-import { EmptyStateComponent } from '../../../shared/ui/empty-state/empty-state.component';
+import { Component, signal } from '@angular/core';
+import { PRODUCTS_DATA } from '../../../data/products.data';
+import { AdminEntity } from '../../../shared/models/admin-entity.model';
+import { AdminAddButtonComponent } from '../../../shared/ui/admin-add-button/admin-add-button.component';
+import { AdminEntityListComponent } from '../../../shared/ui/admin-entity-list/admin-entity-list.component';
+import { GrayLineComponent } from '../../../shared/ui/gray-line/gray-line.component';
 
 @Component({
   selector: 'app-products-page',
-  imports: [EmptyStateComponent],
+  imports: [
+    GrayLineComponent,
+    AdminEntityListComponent,
+    AdminAddButtonComponent,
+  ],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.scss',
 })
-export class ProductsPageComponent {}
+export class ProductsPageComponent {
+  products = signal<AdminEntity[]>(
+    PRODUCTS_DATA.rows.map((product) => ({
+      ...product,
+    }))
+  );
+
+  onEditProduct(product: AdminEntity) {
+    console.log('Edit product:', product);
+  }
+
+  onDeleteProduct(productId: number) {
+    this.products.update((products) =>
+      products.filter((product) => product.id !== productId)
+    );
+    console.log('Delete product with id:', productId);
+  }
+
+  onAddProduct() {
+    console.log('Add new product');
+  }
+}
