ddc6dfb8390c1031b4f113506db53e7545d91f59
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-16 09:19:44 +0100

    feat(admin - users): add users data, add layout and static users to admin panel

--- src/app/pages/admin/users-page/users-page.component.ts
+++ src/app/pages/admin/users-page/users-page.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { USERS } from '../../../data/users.data';
+import { AdminEntity } from '../../../shared/models/admin-entity.model';
+import { AdminAddButtonComponent } from '../../../shared/ui/admin-add-button/admin-add-button.component';
+import { AdminEntityListComponent } from '../../../shared/ui/admin-entity-list/admin-entity-list.component';
+import { GrayLineComponent } from '../../../shared/ui/gray-line/gray-line.component';
 
 @Component({
   selector: 'app-users-page',
-  imports: [],
+  imports: [
+    GrayLineComponent,
+    AdminEntityListComponent,
+    AdminAddButtonComponent,
+  ],
   templateUrl: './users-page.component.html',
-  styleUrl: './users-page.component.scss'
+  styleUrl: './users-page.component.scss',
 })
 export class UsersPageComponent {
+  users = signal<AdminEntity[]>(
+    USERS.map((user) => ({
+      ...user,
+      name: `${user.firstName} ${user.lastName}`,
+    }))
+  );
 
+  onEditUser(user: AdminEntity) {
+    console.log('Edit user:', user);
+  }
+
+  onDeleteUser(userId: number) {
+    this.users.update((users) => users.filter((user) => user.id !== userId));
+    console.log('Delete user with id:', userId);
+  }
+
+  onAddUser() {
+    console.log('Add new user');
+  }
 }
