fc1d7fc1c0e355d7bac1e806eaa0a195ce18e84e
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-16 07:55:56 +0100

    feat(admin - types): add layout and static types

--- src/app/pages/admin/types-page/types-page.component.ts
+++ src/app/pages/admin/types-page/types-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GrayLineComponent } from '../../../shared/ui/gray-line/gray-line.component';
+import { AdminEntityListComponent } from '../../../shared/ui/admin-entity-list/admin-entity-list.component';
+import { AdminAddButtonComponent } from '../../../shared/ui/admin-add-button/admin-add-button.component';
+import { AdminEntity } from '../../../shared/models/admin-entity.model';
+import { TYPES } from '../../../data/types.data';
 
 @Component({
   selector: 'app-types-page',
-  imports: [],
+  imports: [
+    GrayLineComponent,
+    AdminEntityListComponent,
+    AdminAddButtonComponent,
+  ],
   templateUrl: './types-page.component.html',
   styleUrl: './types-page.component.scss',
 })
-export class TypesPageComponent {}
+export class TypesPageComponent {
+  types = signal<AdminEntity[]>([...TYPES]);
+
+  onEditType(type: AdminEntity) {
+    console.log('Edit type:', type);
+  }
+
+  onDeleteType(typeId: number) {
+    this.types.update((types) => types.filter((type) => type.id !== typeId));
+    console.log('Delete type with id:', typeId);
+  }
+
+  onAddType() {
+    console.log('Add new type');
+  }
+}
