1325128b9317f3d6ccf5bd067fb976a08550d84e
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-19 11:00:54 +0100

    feat(admin - dashboard): add dashboard card and dashboard page

--- src/app/pages/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/admin/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ORDERS } from '../../../data/orders.data';
+import { PRODUCTS_DATA } from '../../../data/products.data';
+import { USERS } from '../../../data/users.data';
+import { AdminDashboardCardComponent } from '../../../shared/ui/admin-dashboard-card/admin-dashboard-card.component';
+import { GrayLineComponent } from '../../../shared/ui/gray-line/gray-line.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [GrayLineComponent, AdminDashboardCardComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss',
 })
-export class DashboardPageComponent {}
+export class DashboardPageComponent {
+  stats = signal({
+    totalRevenue: this.calculateTotalRevenue(),
+    totalOrders: ORDERS.length,
+    totalUsers: USERS.length,
+    totalProducts: PRODUCTS_DATA.rows.length,
+  });
+
+  // last 5 orders
+  recentOrders = signal(ORDERS.slice(0, 5));
+
+  private calculateTotalRevenue(): number {
+    return ORDERS.reduce((total, order) => {
+      return total + order.cart.summary.total;
+    }, 0);
+  }
+}
---
+++ src/app/shared/ui/admin-dashboard-card/admin-dashboard-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-dashboard-card',
+  imports: [],
+  templateUrl: './admin-dashboard-card.component.html',
+  styleUrl: './admin-dashboard-card.component.scss',
+})
+export class AdminDashboardCardComponent {
+  title = input<string>('');
+  value = input<string | number>('');
+  icon = input<string>();
+  trend = input<{ value: number; isPositive: boolean }>();
+  color = input<string>('#3b82f6');
+
+  Math = Math;
+}
